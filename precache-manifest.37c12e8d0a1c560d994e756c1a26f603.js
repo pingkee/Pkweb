da845f992122d1aa7ad420b227ecef133934e4b0
PingKee <pingkee.ng@gmail.com>
2020-04-19 16:23:12 +0800

    Updates

---
+++ precache-manifest.37c12e8d0a1c560d994e756c1a26f603.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccf3837c55892be58dde2cd0d2e733a",
+    "url": "/Pkweb.git/index.html"
+  },
+  {
+    "revision": "7f210fabb5025336809d",
+    "url": "/Pkweb.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f8e5d48060f566b0cb2d",
+    "url": "/Pkweb.git/static/js/2.89239fd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pkweb.git/static/js/2.89239fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f210fabb5025336809d",
+    "url": "/Pkweb.git/static/js/main.e3d59050.chunk.js"
+  },
+  {
+    "revision": "d1b2577c8247618555b2",
+    "url": "/Pkweb.git/static/js/runtime-main.f1e1a3e3.js"
+  }
+]);
