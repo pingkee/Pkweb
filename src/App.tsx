fb2830ebaa0c850e01f6582445179eb5068909e1
PingKee <pingkee.ng@gmail.com>
2020-04-19 17:11:37 +0800

    fix: webpage display

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  const header = () => (
+    <div>
+      Ng Ping Kee
+    </div>
+  );
+  return (
+    <div>
+      {header}
+    </div>
+  );
+}
+
+export default App;
