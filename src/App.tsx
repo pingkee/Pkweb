7ed11cc93e066531a13397deea840c7ae8c5f88c
PingKee <pingkee.ng@gmail.com>
2020-07-21 19:37:57 +0800

    refactor: added CNAME and refactor with of display page

--- src/App.tsx
+++ src/App.tsx
@@ -117,6 +117,7 @@ const App: React.FC = () => {
         style={{
           // backgroundColor: '#282c34',
           margin: '0.5rem',
+          width: 800,
           // border: 'solid white 1px',
           // height: '80vh',
           display: 'flex',
