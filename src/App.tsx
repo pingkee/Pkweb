99bd32c2e911fa101f3d95a61ef2702610b39398
PingKee <pingkee.ng@gmail.com>
2020-07-21 23:00:59 +0800

    refactor: changes to fontsize

--- src/App.tsx
+++ src/App.tsx
@@ -15,17 +15,17 @@ const App: React.FC = () => {
       // minHeight: '100vh',
       }}>
         <div style={{
-          fontSize: 'calc(8px + 2vmin)',
+          fontSize: 'calc(14px + 0.5vmin)',
           marginBottom: 10,
           fontWeight: 'bold', }}>
           Current Work
         </div>
         <div style={{
-          fontSize: 'calc(7px + 1vmin)' }}>
+          fontSize: 'calc(12px + 0.5vmin)' }}>
           I am a Front end developer for ST Electronics in Singapore.
-          I am interested in web and mobile development with great passion for
+          I am a hardcore fan of web and mobile development with great passion for
           UI effects, animations and creating intuitive, dynamic user experiences. I build personal
-          webpages and mobile as a freelancer outside my working hours.
+          webpages and mobile apps for leisure outside my working hours.
       </div>
     </div>
   );
@@ -42,27 +42,30 @@ const App: React.FC = () => {
       padding: 15
       // minHeight: '100vh',
       }}>
+        <div style={{ textAlign: 'center'}}>
           <img
             src={require("./public/Profile.png")}
-            alt="My Pic"
-            style={{ height: 150, width: 150, borderRadius: 98, marginBottom: 5, marginLeft: 25}}
+            alt="My Profile Picture"
+            style={{ height: 180, width: 180, borderRadius: 120 }}
             />
+        </div>
         <div style={{
-          fontSize: 'calc(8px + 1vmin)',
+          fontSize: 'calc(16px + 0.5vmin)',
           marginBottom: 10,
           textAlign: 'center',
           fontWeight: 'bold', }}>
           Ng Ping Kee
         </div>
         <div style={{
-          fontSize: 'calc(3px + 1vmin)',
-          marginBottom: 25,
+          fontSize: 'calc(12px + 0.3vmin)',
+          marginBottom: 20,
           fontStyle: 'italic',
+          textAlign: 'center',
           }}>
-          "If the technology is out there, it can be done." 
+          "If the technology is out there, anything can be done." 
         </div>
         <div style={{
-          fontSize: 'calc(7px + 1vmin)'}}>
+          fontSize: 'calc(12px + 0.3min)'}}>
           - Full Stack Developer
           <br />
           - Web/Mobile (IOS & Android)
@@ -77,23 +80,36 @@ const App: React.FC = () => {
       margin: '0.5rem',
       backgroundColor: 'white',
       border: 'solid lightblue 2px',
-      textAlign: 'justify',
+      // textAlign: 'justify',
       width: 400,
       padding: 15
       // minHeight: '100vh',
       }}>
         <div style={{
-          fontSize: 'calc(8px + 2vmin)',
+          fontSize: 'calc(16px + 0.5vmin)',
           marginBottom: 10,
           fontWeight: 'bold', }}>
           My Experience as a Developer
         </div>
         <div style={{
-          fontSize: 'calc(7px + 1vmin)'}}>
-          Full stack developer at ST Electronics (Infosoft)<br />
-          Design build and manage a custom web app used by the gov.
-          Also worked with infosys developing fincale API for DBS Bank Siingapore<br />
-          Privately build Web App and Mobile Apps.
+          fontSize: 'calc(12px + 0.5vmin)'}}>
+          
+          <ul style={{padding: 0}}>
+            1. Software Developer at ST Electronics (Current)
+            <ol>
+              - Work with various team to design, build and manage a custom webapp (Full Stack)
+            </ol>
+          </ul>
+          <ul style={{padding: 0}}>
+            2. Worked with Infosys to build backend products for DBS Bank Siingapore
+            <ol>
+              - developing fincale API<br />
+              - data migration<br />
+            </ol>
+          </ul>
+          <ul style={{padding: 0}}>
+            3. Privately build Web App and Mobile Apps for leisure
+          </ul>
       </div>
     </div>
   );
@@ -109,13 +125,13 @@ const App: React.FC = () => {
       // minHeight: '100vh',
       }}>
         <div style={{
-          fontSize: 'calc(8px + 2vmin)',
+          fontSize: 'calc(16px + 0.5vmin',
           marginBottom: 10,
           fontWeight: 'bold', }}>
           Current Stack
         </div>
         <div style={{
-          fontSize: 'calc(7px + 1vmin)'}}>
+          fontSize: 'calc(12px + 0.5vmin)'}}>
             - ReactJS, React Native, HTML, CSS, Javascript, nodeJs<br />
             - FeathersJS, PHP, MongoDB, MySQL
       </div>
