b4440bd9702c2956b77317e8f6ca429a5d973a86
PingKee <pingkee.ng@gmail.com>
2020-07-21 23:03:12 +0800

    fix: profilepage fontsize off

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const App: React.FC = () => {
           Ng Ping Kee
         </div>
         <div style={{
-          fontSize: 'calc(12px + 0.3vmin)',
+          fontSize: 'calc(11px + 0.4vmin)',
           marginBottom: 20,
           fontStyle: 'italic',
           textAlign: 'center',
