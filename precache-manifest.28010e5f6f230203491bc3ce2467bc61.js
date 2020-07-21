9b4b9f9fab58b40026c0050f73d4b24efe95ecd4
PingKee <pingkee.ng@gmail.com>
2020-07-21 19:28:15 +0800

    Updates

---
+++ precache-manifest.28010e5f6f230203491bc3ce2467bc61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779993b944548c0b6c7f75875dc03506",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fc1c7cd60e34e3f7da3",
+    "url": "/static/css/main.309ead0f.chunk.css"
+  },
+  {
+    "revision": "f8e5d48060f566b0cb2d",
+    "url": "/static/js/2.89239fd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89239fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc1c7cd60e34e3f7da3",
+    "url": "/static/js/main.3d7c6084.chunk.js"
+  },
+  {
+    "revision": "e8807207f91b8ffa3524",
+    "url": "/static/js/runtime-main.34922f11.js"
+  },
+  {
+    "revision": "d3166ee32fbd1be65d071c022551a76c",
+    "url": "/static/media/Profile.d3166ee3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pkweb/precache-manifest.e9e638c06e8659d93eac77660a6bd10f.js"
+  "/precache-manifest.28010e5f6f230203491bc3ce2467bc61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Pkweb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
